cc60ddaebbe88cc884d540c4d5b34e0a3700f80c
RinatMiniazev <r.miniazev@gmail.com>
2023-08-14 15:43:39 +0300

    added one log message to stdout

--- app/src/main/java/com/example/helloappjava/MainActivity.java
+++ app/src/main/java/com/example/helloappjava/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: started");
+        Log.d(TAG, "onCreate: started from github");
+
     }
 
     @Override
